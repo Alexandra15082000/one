25cb28831a063802531dcd19add6e75adebd4922
Alexandra <nazarova.sasha.2000@mail.ru>
2020-11-29 01:31:36 +0300

    add function MyMul()

--- program.cpp
+++ program.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 int main()
 {
-	cout << MySum(10, 3) << endl;
-	cout << MySub(10, 3) << endl;
-	“system("pause");
-	return 0;
+cout << MySum(10, 3) << endl;
+cout << MySub(10, 3) << endl;
+cout << MyMul(10, 3) << endl;
+system("pause");
+return 0;
 }
+
 
 
